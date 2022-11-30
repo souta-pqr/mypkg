3f2c5f7484b9a10c8f66f226dd56d5949f4f1db4
Souta Kobori <s21c1048fl@s.chibakoudai.jp>
2022-11-30 21:14:38 +0900

    modified talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
+
+def cb(request, response):
+	if request.name == "小堀聡太":
+		response.age = 19
+	else:
+		response.age = 255
+
+	return response
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
-
-def cb():
-	global n
-	msg = Person()
-	msg.name = "小堀聡太"
-	msg.age = n
-	pub.publish(msg)
-	n += 1
-
-node.create_timer(0.5, cb)
+srv = node.create_service(Query, "query", cb)
 rclpy.spin(node)
