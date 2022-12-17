750f5e2cfe03286e048ecd7ffb3b4e9ff124c8aa
Souta Kobori <s21c1048fl@s.chibakoudai.jp>
2022-12-17 11:25:18 +0900

    modified talker

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -13,30 +13,7 @@ def cb(request, response):
 
 	return response
 
-<<<<<<< HEAD
 rclpy.init()
 node = Node("talker")
 srv = node.create_service(Query, "query", cb)
 rclpy.spin(node)
-=======
-class Talker():
-	def __init__(self, node):
-		self.pub = node.create_publisher(Int16, "countup", 10)
-		self.n = 0
-		node.create_timer(0.5, self.cb)
-
-	def cb(self):
-		msg = Int16()
-		msg.data = self.n
-		self.pub.publish(msg)
-		self.n += 1
-
-def main():
-	rclpy.init()
-	node = Node("talker")
-	talker = Talker(node)
-	rclpy.spin(node)
-
-if __name__ == '__main__':
-	main()
->>>>>>> lesson10
